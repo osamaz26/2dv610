02b0cb028a0b292e4083ee02a3f208a5a4854c0b
Jacob Yousif <jy222bz@student.lnu.se>
2019-11-27 02:19:53 +0100

    * COVERAGE

--- tests/PersonalNumberTest.java
+++ tests/PersonalNumberTest.java
@@ -1,10 +1,10 @@
 import exceptions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class PersonalNumberTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonalNumberTest {
 
     private PersonalNumber sut;
 
@@ -20,6 +20,26 @@ public class PersonalNumberTest {
     }
 
     @Test
+    void shouldThrowInvalidFormatExceptionWhenTheSizeIsAboveTheLimit() {
+        String testData = "19870412-4612";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+
+    @Test
+    void shouldThrowInvalidFormatExceptionWhenEmpty() {
+        String testData = "";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+    @Test
+    void shouldThrowInvalidFormatExceptionWhenThereIsNoHyphenButSomethingElse() {
+        String testData = "870412*4612";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+
+    @Test
     void shouldThrowInvalidFormatExceptionWhenThereIsNoHyphen() {
         String testData = "8714124612";
         assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
@@ -28,6 +48,24 @@ public class PersonalNumberTest {
     @Test
     void shouldThrowInvalidDateExceptionWhenThereIsWhiteSpaceInTheDate() {
         String testData = "87  12-4612";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+    @Test
+    void shouldThrowInvalidDateExceptionWhenTheDaysPassTheLimitOfTheMonth() {
+        String testData = "20100931-6164";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+    @Test
+    void shouldThrowInvalidDateExceptionWhenTheYearIsNotLeapYear() {
+        String testData = " 970229-3797";
+        assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
+    }
+
+    @Test
+    void shouldThrowInvalidDateExceptionWhenTheDayNotCorrect() {
+        String testData = " 970029-3797";
         assertThrows(PersonalNumberInvalidFormatException.class, () -> sut.setValue(testData));
     }
 
@@ -45,10 +83,26 @@ public class PersonalNumberTest {
     }
 
     @Test
+    void shouldThrowInvalidDateExceptionWhenTheDayIs31() {
+        String testData = "870431-4612";
+        assertThrows(PersonalNumberInvalidDateException.class, () -> sut.setValue(testData));
+    }
+
+    @Test
+    void shouldThrowInvalidDateExceptionWhenTheDayIs00() {
+        String testData = "870400-4612";
+        assertThrows(PersonalNumberInvalidDateException.class, () -> sut.setValue(testData));
+    }
+
+
+
+
+    @Test
     void shouldThrowInvalidCheckSumException() {
         String testData = "870412-0000";
         assertThrows(PersonalNumberInvalidChecksumException.class, () -> sut.setValue(testData));
     }
+
 
     @Test
     void shouldReturnTheExpectedValue() {
