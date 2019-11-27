3813e32f415f6134682b9d87897edab6e650a2c4
Jacob Yousif <jy222bz@student.lnu.se>
2019-11-27 11:40:13 +0100

    * COVERAGE

--- tests/NameTest.java
+++ tests/NameTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class NameTest {
+class NameTest {
 
     private Name sut;
 
