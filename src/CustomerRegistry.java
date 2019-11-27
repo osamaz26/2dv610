10842310d4ef45cfad8d0e854db79cb8a9ed23c9
Jacob Yousif <jy222bz@student.lnu.se>
2019-11-27 17:16:51 +0100

    * RED

---
+++ src/CustomerRegistry.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class CustomerRegistry {
+
+    public void add(Customer customer) {
+    }
+
+    public void delete(int id) {
+    }
+
+    public List<Customer> getList() {
+        return null;
+    }
+
+    public Customer retrieve(int id) {
+        return null;
+    }
+}
---
+++ tests/CustomerRegistryTest.java
@@ -0,0 +1,95 @@
+import exceptions.CustomerNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class CustomerRegistryTest {
+    private CustomerRegistry sut;
+    private Customer mockCustomer;
+
+    @BeforeEach
+    void beforeEach() {
+        sut = new CustomerRegistry();
+        mockCustomer = mock(Customer.class);
+    }
+
+    @Test
+    void shouldThrowCustomerNotFoundException() {
+        Customer testData = new Customer();
+        sut.add(testData);
+        int nonExistentID = 1009;
+        assertThrows(CustomerNotFoundException.class, () -> sut.delete(nonExistentID));
+    }
+
+    @Test
+    void shouldThrowCustomerNotFoundExceptionOnRetrieve() {
+        Customer testData = new Customer();
+        sut.add(testData);
+        int nonExistentID  = 101;
+        assertThrows(CustomerNotFoundException.class, () -> sut.retrieve(nonExistentID));
+    }
+
+    @Test
+    void shouldRetrieveTheFirstCustomerById() {
+        Customer expected = new Customer();
+        sut.add(expected);
+        int firstIDForTheFirstCustomer  = 1;
+        Customer actual = sut.retrieve(firstIDForTheFirstCustomer);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRetrieveTheFifthCustomerById() {
+        Customer expected = new Customer();
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(expected);
+        int idForTheTarget  = 5;
+        Customer actual = sut.retrieve(idForTheTarget);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldDeleteTheFifthCustomerAndThrowExceptionWhenToRetrieveIt() {
+        Customer testData = new Customer();
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(new Customer());
+        sut.add(testData);
+        int idForTheTarget = 7;
+        sut.delete(idForTheTarget);
+        assertThrows(CustomerNotFoundException.class, () -> sut.retrieve(idForTheTarget));
+    }
+
+    @Test
+    void shouldReturnTheListOfCustomersCustomers() {
+        Customer testData1 = new Customer();
+        Customer testData2 = new Customer();
+        Customer testData3 = new Customer();
+        sut.add(testData1);
+        sut.add(testData2);
+        sut.add(testData3);
+        List<Customer> expected = new ArrayList<>();
+        expected.add(testData1);
+        expected.add(testData2);
+        expected.add(testData3);
+        List<Customer> actual = sut.getList();
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldInteractWithTheCustomerObjectUponSettingID() {
+        sut.add(mockCustomer);
+        verify(mockCustomer, times(1)).getId();
+    }
+}
