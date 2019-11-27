de8e558eb1f4b1c39c2f5066edcbf4bd283be01e
Jacob Yousif <jy222bz@student.lnu.se>
2019-11-27 17:31:30 +0100

    * GREEN

--- src/CustomerRegistry.java
+++ src/CustomerRegistry.java
@@ -1,18 +1,39 @@
+import exceptions.CustomerNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CustomerRegistry {
 
+    private HashMap<Integer, Customer> list = new HashMap<>();
+    private int index = 1;
+
     public void add(Customer customer) {
+        customer.setId(index);
+        index++;
+        int temp = customer.getId();
+        list.put(temp, customer);
     }
 
     public void delete(int id) {
+        if (list.get(id) == null) {
+            throw new CustomerNotFoundException();
+        } else {
+            list.remove(id);
+        }
     }
 
     public List<Customer> getList() {
-        return null;
+        ArrayList tempList = new ArrayList<>(list.values());
+        return tempList;
     }
 
     public Customer retrieve(int id) {
-        return null;
+        var customer = list.get(id);
+        if (customer == null) {
+            throw new CustomerNotFoundException();
+        } else {
+            return customer;
+        }
     }
 }
--- tests/CustomerRegistryTest.java
+++ tests/CustomerRegistryTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-public class CustomerRegistryTest {
+class CustomerRegistryTest {
     private CustomerRegistry sut;
     private Customer mockCustomer;
 
