18796d0b2df65c5d4852661d8b90d0d987d4bfa6
Osama Zarraa <osamaz26@gmail.com>
2019-11-29 16:03:37 +0100

    * GREEN

--- src/View.java
+++ src/View.java
@@ -1,48 +1,95 @@
 import exceptions.CustomerNotFoundException;
 
 public class View {
+    private CustomerRegistry registry;
+    private Console console;
 
     public View(Console console, CustomerRegistry registry) {
-
-    }
-
-    public void run() {
-
+        this.registry = registry;
+        this.console = console;
     }
 
     public int readMenu() {
-        return 0;
-    }
-
-    public void addCustomer() {
-
+        var str = "============================\n" +
+                "1: Add customer\n" +
+                "2: delete customer\n" +
+                "3: list customer\n" +
+                "4: retrieve customer\n" +
+                "0: exit\n" +
+                "============================";
+        console.writeLine(str);
+        return readIntWithMessage("Your choice: ");
     }
 
     public Name readName() {
-        return null;
+        var name = new Name();
+        try {
+            var value = readLineWithMessage("Name: ");
+            name.setValue(value);
+        } catch (Exception exception) {
+            console.writeLine("Exception: " + exception.toString());
+            return null;
+        }
+        return name;
     }
 
     public PersonalNumber readPersonalNumber() {
-        return null;
+        var personalNumber = new PersonalNumber();
+        try {
+            personalNumber.setValue(readLineWithMessage("Personal number: "));
+        } catch (Exception exception) {
+            console.writeLine("Exception: " + exception.toString());
+            return null;
+        }
+        return personalNumber;
     }
 
     public void deleteCustomer() {
+        console.writeLine("Delete customer");
+        var id = readIntWithMessage("Customer Id: ");
 
+        try {
+            registry.delete(id);
+        } catch (Exception exception) {
+            console.writeLine("Exception: " + exception.toString());
+        }
     }
 
     public void listCustomers() {
-
+        console.writeLine("List of customers");
+        var list = registry.getList();
+        for (var customer : list) {
+            console.writeLine("[" + customer.getId() + "] " +
+                    customer.getNameAsString() + " " +
+                    customer.getPersonalNumberAsString());
+        }
     }
 
     public void retrieveCustomer() {
+        console.writeLine("Retrieve customer");
+        var id = readIntWithMessage("Customer Id: ");
 
+        Customer customer;
+        try {
+            customer = registry.retrieve(id);
+        } catch (Exception exception) {
+            console.writeLine("Exception: " + exception.toString());
+            return;
+        }
+        if (customer == null) {
+            throw new CustomerNotFoundException();
+        }
+        console.writeLine("[" + customer.getId() + "] " + customer.getNameAsString() + " " +
+                customer.getPersonalNumberAsString());
     }
 
     public int readIntWithMessage(String message) {
-        return 0;
+        console.write(message);
+        return console.readInt();
     }
 
     public String readLineWithMessage(String message) {
-        return "";
+        console.write(message);
+        return console.readLine();
     }
 }
