53e4d63abc536d57f38c7357993294a369fc85dd
Osama Zarraa <osamaz26@gmail.com>
2019-11-29 09:59:47 +0100

    * REFACTOR

--- src/Console.java
+++ src/Console.java
@@ -3,37 +3,63 @@ import exceptions.ConsoleReadException;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+/**
+ * The type Console.
+ */
 public class Console {
     private BufferedReader bufferedReader;
 
+    /**
+     * Instantiates a new Console.
+     *
+     * @param bufferedReader the buffered reader
+     */
     public Console(BufferedReader bufferedReader) {
         this.bufferedReader = bufferedReader;
     }
 
+    /**
+     * Read line string.
+     *
+     * @return the string
+     */
     public String readLine() {
         try {
-            var line = bufferedReader.readLine();
-            return line;
+            return bufferedReader.readLine();
         } catch (IOException e) {
             throw new ConsoleReadException();
         }
     }
 
-    public int readInt(){
+    /**
+     * Read int int.
+     *
+     * @return the int
+     */
+    public int readInt() {
         String str;
         try {
             str = bufferedReader.readLine();
         } catch (IOException e) {
             throw new ConsoleReadException();
         }
-        var a = Integer.parseInt(str);
-        return a;
+        return Integer.parseInt(str);
     }
 
+    /**
+     * Write line.
+     *
+     * @param line the line
+     */
     public void writeLine(String line) {
         System.out.println(line);
     }
 
+    /**
+     * Write.
+     *
+     * @param line the line
+     */
     public void write(String line) {
         System.out.print(line);
     }
