14cbde2453826ba1ce3c93bc2011b899b40c24b8
Osama Zarraa <osamaz26@gmail.com>
2019-11-29 09:58:48 +0100

    * GREEN

--- src/Console.java
+++ src/Console.java
@@ -1,25 +1,41 @@
+import exceptions.ConsoleReadException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Console {
+    private BufferedReader bufferedReader;
 
     public Console(BufferedReader bufferedReader) {
-
+        this.bufferedReader = bufferedReader;
     }
 
     public String readLine() {
-        return "";
+        try {
+            var line = bufferedReader.readLine();
+            return line;
+        } catch (IOException e) {
+            throw new ConsoleReadException();
+        }
     }
 
     public int readInt(){
-        return 0;
+        String str;
+        try {
+            str = bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new ConsoleReadException();
+        }
+        var a = Integer.parseInt(str);
+        return a;
     }
 
     public void writeLine(String line) {
-
+        System.out.println(line);
     }
 
     public void write(String line) {
-
+        System.out.print(line);
     }
 
 }
